31eccbefc914d41ea1694fd789092d2562924a96
Aminat Ahmed <oahmedaminat@gmail.com>
2022-08-22 22:00:04 +0100

    update fixes

--- src/Success.js
+++ src/Success.js
@@ -8,7 +8,7 @@ const Success = () => {
     <div className='home'>
         <Card className="success-card"> 
             <Card.Body>
-                <FcCheckmark style={{width: '50px', height:'50px', marginTop:'15px'}}/>
+                <FcCheckmark style={{width: '50px', height:'50px', marginTop:'35px', background: '#FFFFFF', boxShadow: '0px 6px 10px rgba(33, 30, 67, 0.08)', borderRadius:'50%'}}/>
                 <h2>Purchase Completed</h2>
                 <p className='info'>Please check your email for details concerning this transaction</p>
                 <Link to="/" className='back'>Return Home</Link>
